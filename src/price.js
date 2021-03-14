c96ba6af5570fd7440e3e56962588209a92fadbb
Janet Owen <janetowen07@gmail.com>
2021-03-14 23:16:15 +0000

    add code to find the cost of 1 apple

--- src/price.js
+++ src/price.js
@@ -1,1 +1,19 @@
-export const calculate = () => null;
+let products = {
+  'A99': 50,
+  'B15': 30,
+  'C40': 60,
+  'T34': 99
+}
+
+export const calculate = (sku, quantity) => {
+
+  let total = 0;
+  
+  for(let item in products) {
+     if (item === sku) {
+       total = quantity * products[item]
+     }
+  }
+
+  return total;
+};
--- test/price.test.js
+++ test/price.test.js
@@ -1,7 +1,7 @@
-import calculate from '../src/price';
+import { calculate } from '../src/price';
 
 describe('calculate prices', function () {
   it('should calculate the cost of an apple', () => {
-    expect(calculate('A99')).toBe(50);
+    expect(calculate('A99', 1)).toBe(50);
   });
 });
