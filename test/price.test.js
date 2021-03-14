aa8ba499c721211719afafd77f9a0e412a5b086d
Janet Owen <janetowen07@gmail.com>
2021-03-14 23:17:21 +0000

    add test for 2 apples

--- test/price.test.js
+++ test/price.test.js
@@ -4,4 +4,7 @@ describe('calculate prices', function () {
   it('should calculate the cost of an apple', () => {
     expect(calculate('A99', 1)).toBe(50);
   });
+  it('should calculate the cost of 2 apples', () => {
+    expect(calculate('A99', 2)).toBe(100);
+  });
 });
