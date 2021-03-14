37c05a678d62217341ab19f5bd80f6e0bb1e3bb5
Janet Owen <janetowen07@gmail.com>
2021-03-14 23:39:52 +0000

    adds logic to calculate the special offer and extra items

--- src/price.js
+++ src/price.js
@@ -1,3 +1,4 @@
+// products object
 let products = {
   'A99': 50,
   'B15': 30,
@@ -5,20 +6,56 @@ let products = {
   'T34': 99
 }
 
+// special offers object
+let offers = {
+  'A99': {
+    quantity: 3,
+    price: 130
+  },
+  'B15': {
+    quantity: 2,
+    price: 45
+  }
+}
+
+/**
+ * This function calculates cost of the number of special offers and the remaining items.
+ * 
+ * @param {*} sku 
+ * @param {*} quantity
+ * @returns result object
+ */
+let getTotalOffer = (sku, quantity) => {
+  let result = {
+    totalOffer: 0,
+    looseItems: quantity
+  }
+
+  let skuOffer = offers[sku];
+
+  if (skuOffer && quantity >= skuOffer.quantity) {
+    let numberOfMultiples = Math.floor( quantity / skuOffer.quantity);
+    result.looseItems = quantity % skuOffer.quantity;
+    result.totalOffer = skuOffer.price * numberOfMultiples;
+  }
+
+  return result;
+}
+
+/**
+ * This calculates the total cost of items.
+ * 
+ * @param {*} sku 
+ * @param {*} quantity
+ * @returns The total cost of items
+ */
 export const calculate = (sku, quantity) => {
 
   let total = 0;
 
-  if (quantity % 3 === 0 & sku === 'A99') {
-    let numberOfMultiples = quantity / 3;
-    return total = numberOfMultiples * 130;
-  }
-
-  for(let item in products) {
-    if (item === sku) {
-      total = quantity * products[item]
-    }
-  }
+  let calculatedResults = getTotalOffer(sku, quantity);
+  total += products[sku] * calculatedResults.looseItems;
+  total += calculatedResults.totalOffer;
 
   return total;
 };
